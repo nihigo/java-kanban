2b65b300d7f1b66ac3f0b93fca60187f71596f5e
nihigo <kudryavtsevalexey1@yandex.ru>
2025-01-20 18:42:34 +0300

    Change InMemoryHistoryManager - use TaskLinkedHashMap ; Update tests

--- src/taskmanager/historymanager/InMemoryHistoryManager.java
+++ src/taskmanager/historymanager/InMemoryHistoryManager.java
@@ -6,89 +6,64 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    public static class TaskLinkedHashMap<T extends Task> {
-        HashMap<Integer, Node<T>> nodesById = new HashMap<>();
-        Node<T> head;
-        Node<T> tail;
+    HashMap<Integer, Node<Task>> nodesById = new HashMap<>();
+    Node<Task> head;
+    Node<Task> tail;
 
-        public Node<T> removeNode(Node<T> node) {
-            if (node == null) {
-                return null;
-            }
-            nodesById.remove(node.data.getId());
-            if (node != head) {
-                node.prev.next = node.next;
-            } else {
-                head = node.next;
-                if (head != null) {
-                    head.prev = null;
-                }
-            }
-            if (node != tail) {
-                node.next.prev = node.prev;
-            } else {
-                tail = node.prev;
-                if (tail != null) {
-                    tail.next = null;
-                }
-            }
-            return node;
+    public Node<Task> removeNode(Node<Task> node) {
+        if (node == null) {
+            return null;
         }
-
-        public void linkLast(T task) {
-            removeNode(nodesById.get(task.getId()));
-
-            Node<T> added = new Node<>(task, null, null);
-            nodesById.put(task.getId(), added);
-
-            if (tail == null) {
-                head = added;
-                tail = head;
-            } else {
-                added.prev = tail;
-                tail.next = added;
-                tail = added;
+        nodesById.remove(node.data.getId());
+        if (node != head) {
+            node.prev.next = node.next;
+        } else {
+            head = node.next;
+            if (head != null) {
+                head.prev = null;
             }
         }
-
-        public ArrayList<T> getTasks() {
-            final ArrayList<T> result = new ArrayList<>();
-            Node<T> temp = head;
-            while (temp != null) {
-                result.add(temp.data);
-                temp = temp.next;
+        if (node != tail) {
+            node.next.prev = node.prev;
+        } else {
+            tail = node.prev;
+            if (tail != null) {
+                tail.next = null;
             }
-            return result;
         }
-
-        public int size() {
-            return nodesById.size();
-        }
-
-        public Node<T> getNodeById(int id) {
-            return nodesById.get(id);
-        }
-    }
-
-    private final TaskLinkedHashMap<Task> history = new TaskLinkedHashMap<>();
-
-    @Override
-    public void add(Task task) {
-        history.linkLast(task);
-    }
-
-    @Override
-    public ArrayList<Task> getHistory() {
-        return history.getTasks();
+        return node;
     }
 
     @Override
     public void remove(int id) {
-        history.removeNode(history.getNodeById(id));
+        removeNode(nodesById.get(id));
     }
 
-    public Node<Task> removeNode(Node<Task> node) {
-        return history.removeNode(node);
+    @Override
+    public void add(Task task) {
+        removeNode(nodesById.get(task.getId()));
+
+        Node<Task> added = new Node<>(task, null, null);
+        nodesById.put(task.getId(), added);
+
+        if (tail == null) {
+            head = added;
+            tail = head;
+        } else {
+            added.prev = tail;
+            tail.next = added;
+            tail = added;
+        }
     }
 
+    @Override
+    public ArrayList<Task> getHistory() {
+        final ArrayList<Task> result = new ArrayList<>();
+        Node<Task> temp = head;
+        while (temp != null) {
+            result.add(temp.data);
+            temp = temp.next;
+        }
+        return result;
+    }
 }
